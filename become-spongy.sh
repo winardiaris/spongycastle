e4cbbd6387a711337ae13c645ab84ffbd1664e5f
Roberto Tyley <roberto.tyley@gmail.com>
2017-04-14 15:20:02 +0100

    Handle additional modules introduced into Bouncy Castle

--- become-spongy.sh
+++ become-spongy.sh
@@ -4,23 +4,9 @@
     
 find -name bouncycastle | xargs rename s/bouncycastle/spongycastle/
 
-find {core,jce,prov,pg,pkix} -type f | xargs -I '{}' ssed -i -R 's/bouncycastle(?!.org)/spongycastle/g' '{}'
-
-# find bc* -type f | xargs sed -i s/bouncycastle/spongycastle/g
+find {core,jce,kmip,misc,pg,pkix,prov,tls} -type f | xargs -I '{}' ssed -i -R 's/bouncycastle(?!.org)/spongycastle/g' '{}'
 
 # BC to SC for provider name
     
-find {core,jce,prov,pg,pkix} -type f | xargs -I '{}' sed -i s/\"BC\"/\"SC\"/g '{}'
-
-# Rename 'bc' artifacts to 'sc'
-    
-# rename s/^bc/sc/ *
-# find -name 'pom.xml' | xargs sed -i s/\>bc/\>sc/g
-
-# Rename maven artifact 'names' to use Spongy rather than Bouncy
-
-# find -name 'pom.xml' | xargs sed -i s/\>Bouncy/\>Spongy/g
-
-    
-
+find {core,jce,kmip,misc,pg,pkix,prov,tls} -type f | xargs -I '{}' sed -i s/\"BC\"/\"SC\"/g '{}'
 
